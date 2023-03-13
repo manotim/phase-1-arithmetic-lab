37a27d83cf815149b2f9cc78e3101a1d105c301e
manoti <manotimike@gmail.com>
2023-03-13 22:41:38 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+const num1 = 2
+const num2 = 31
+
+const multiply = num1 * num2
+
+const random = Math.floor(Math.random()) + 1
+
+const num3 = 5
+const num4 = 19
+const mod = num4 % num3
+
+const max = Math.max(2, 3, 4, 20)
